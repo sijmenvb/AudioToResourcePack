05dca962a96f5c58194a8a2da0e6d73a2a19b0d2
sijmen_v_b <sijmenvanbommel@gmail.com>
2021-12-30 02:59:20 +0100

    demo to convert any file to .ogg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,49 @@
+import java.io.File;
+
+import ws.schild.jave.Encoder;
+import ws.schild.jave.MultimediaObject;
+import ws.schild.jave.encode.AudioAttributes;
+import ws.schild.jave.encode.EncodingAttributes;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("running..");
+
+		// variables
+		final String inFilePath = "src/main/resources/conversation engine.mp4";
+		final String outFilePath = "src/main/resources/output2.ogg";
+		final String oggFormat = "ogg";
+		final String oggCodec = "libvorbis";
+		
+		try {
+			File source = new File(inFilePath);
+			File target = new File(outFilePath);
+			
+			System.out.println("does the in file exist:" + source.exists());
+			System.out.println("does the out file exist:" + target.exists());
+
+			// set the audio attributes (see https://github.com/a-schild/jave2 and
+			// https://codereview.stackexchange.com/questions/20084/converting-audio-to-different-file-formats)
+			AudioAttributes audioAttr = new AudioAttributes();
+			audioAttr.setBitRate(128000);
+			audioAttr.setChannels(2);
+			audioAttr.setSamplingRate(44100);
+			audioAttr.setCodec(oggCodec);
+
+			// set the encoder
+			EncodingAttributes encoAttrs = new EncodingAttributes();
+			encoAttrs.setOutputFormat(oggFormat);
+			encoAttrs.setAudioAttributes(audioAttr);
+
+			// Encode
+			Encoder encoder = new Encoder();
+			encoder.encode(new MultimediaObject(source), target, encoAttrs);
+			System.out.println("does the out file exist:" + target.exists());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+
+		}
+
 	}
 }
